e11fbda2aa35ccebc14bfb1120cf206750927dbd
Himel Rana <profession@himelrana-swe.com>
2020-07-01 06:18:35 +0600

    Publishing to github page

---
+++ precache-manifest.f77812c6cd51911d2ed56ebc904dfd60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64b95954360e7aa35ba6716a05f5f08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5e62443a7d6d09ef1e5",
+    "url": "/static/css/main.180bd231.chunk.css"
+  },
+  {
+    "revision": "b10b3b21f8dfa9b3e56a",
+    "url": "/static/js/2.179be52c.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.179be52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e62443a7d6d09ef1e5",
+    "url": "/static/js/main.53c8d74b.chunk.js"
+  },
+  {
+    "revision": "355f4332e4e83b06955d",
+    "url": "/static/js/runtime-main.8eeaf200.js"
+  },
+  {
+    "revision": "1dc8b4b367de9e6f2136db3edfb86548",
+    "url": "/static/media/animation.1dc8b4b3.gif"
+  },
+  {
+    "revision": "bb533f76423cab3aa8f798501357e763",
+    "url": "/static/media/loading.bb533f76.gif"
+  },
+  {
+    "revision": "1466281417e7844c31ca6e2e86f76f67",
+    "url": "/static/media/tick.14662814.png"
+  }
+]);
